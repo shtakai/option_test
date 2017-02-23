0270a34192fa39cb989d628b649601cad10c4b79
Alyson T <shtakai@gmail.com>
2017-02-23 10:46:32 +0900

    added

--- main.js
+++ main.js
@@ -12,10 +12,11 @@ function countSelected(clicked_,select_id,limit) {
   console.log('selected');
   var count=0;
   var option_elm = $(select_id).children();
-  for(var i=0;i<option_elm.length;i++)
-    if(option_elm[i].selected)count++
+  for(var i=0;i<option_elm.length;i++){
+    if(option_elm[i].selected)count++;
+  }
   if(count>limit) {
-    alert("選択は"+limit+"つまでです")
+    alert("選択は"+limit+"つまでです");
     $(clicked_).removeAttr('selected');
   }
 
